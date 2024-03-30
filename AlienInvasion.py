aecad53c8f75145a6f2b1c914d798b00f578319b
aly <alyasyly8668@gmail.com>
2024-03-30 01:18:58 -0700

    resetting the game

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -68,7 +68,17 @@ class AlienInvasion:
     def _check_play_botton(self,mouse_pos):
         """Start a new game when the player clicks Play."""
         if self.play_button.rect.collidepoint(mouse_pos):
+            # Reset the game statistics.
+            self.stats.reset_stats()
             self.game_active = True
+
+            # Get rid of any remaining bullets and aliens.
+            self.bullets.empty()
+            self.aliens.empty()
+
+            # create a new fleet and cecter the ship
+            self._create_fleet()
+            self.ship.center_ship()
 
     def _check_keydown_events(self,event):
         """Respond to keypresses"""
