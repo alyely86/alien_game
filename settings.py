986a72e374352126489de38b0c9239e309b629cb
aly <alyasyly8668@gmail.com>
2024-04-05 13:27:22 -0700

    modify __init__ func and add initialize_dynamic_settings func

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@
 class Settings:
     """A class to store all settings for Alien Invasion."""
     def __init__(self):
-        """Initialize the game's settings."""
+        """Initialize the game's static settings."""
         #Screen settings
         self.screen_width = 650
         self.screen_height = 800
@@ -16,13 +16,24 @@ class Settings:
 
         # Bullet settings
         self.bullet_speed = 2.5
-        self.bullet_width = 300
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color  = (60,60,60)
         self.bullet_allowed = 3
 
         # Alien settings
         self.alien_speed = 2.0
-        self.fleet_drop_speed = 100
-        #fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_drop_speed = 10
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """initialize settings that change throughout the game"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.5
+        self.alien_speed = 1.0
+
+        ## fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
